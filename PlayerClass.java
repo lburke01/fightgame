cc1d3d50d7cadff46265f9da4501eb22e602353b
lburke01 <lburkerobotics@gmail.com>
2018-05-21 10:24:39 -0400

    Created PlayerClass
    Accesses stats of each player

---
+++ PlayerClass.java
@@ -0,0 +1,11 @@
+//accessors for player stats
+//created for each player
+public abstract class PlayerClass
+{
+  abstract int getHealthStat();
+  abstract int getManaStat();
+  abstract int getStrengthStat();
+  abstract int getDefenseStat();
+  abstract int getMagicStat();
+  abstract int getSpeedStat();
+}
