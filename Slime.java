b53035035bbb6e748089e2276adbe48d68f8b026
FinnFlam <39463740+FinnFlam@users.noreply.github.com>
2018-05-23 10:24:33 -0400

    im so fking good

--- Slime.java
+++ Slime.java
@@ -7,9 +7,9 @@ public class Slime extends Enemy
     int speed;
     public Slime(int level)
     {
-      maxHealth = 7 + (Math.floor((Math.random()*2)+2);
+      maxHealth = 7 + (level*2) + (int)((level*Math.Random())+1)
       currentHealth = maxHealth;
-      strength;
+      strength=;
       defense;
       speed;
     }
