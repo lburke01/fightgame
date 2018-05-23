b8d4ae12f03c08a3fe5ee9fdf99a701994b11539
uayevan01 <35662789+uayevan01@users.noreply.github.com>
2018-05-23 10:24:18 -0400

    studff

--- ActorClass.java
+++ ActorClass.java
@@ -1,11 +1,12 @@
 //accessors for player stats
 //created for each player
-public abstract class ActorClass
+public class ActorClass
 {
-  abstract int getHealthStat();
-  abstract int getManaStat();
-  abstract int getStrengthStat();
-  abstract int getDefenseStat();
-  abstract int getMagicStat();
-  abstract int getSpeedStat();
+  int getName();
+  int getHealthStat();
+  int getManaStat();
+  int getStrengthStat();
+  int getDefenseStat();
+  int getMagicStat();
+  int getSpeedStat();
 }
