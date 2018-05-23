05252bbc48d2835624f13e3eb4819107554e8e68
lburke01 <lburkerobotics@gmail.com>
2018-05-22 20:18:30 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -15,7 +15,7 @@ public class Player extends Actor
   }
   public void changeHealth(int change)
   {
-    if(currentHealth + change > maxHealth)
+    if(currentHealth + change > maxHealth) //Prevents over-healing by setting currentHealth to maxHealth if the sum is greater than maxHealth
       currentHealth = maxHealth;
     else
       currentHealth += change; //if change is damage, it is a negative value; if change is healing, it is a positive value
@@ -23,6 +23,9 @@ public class Player extends Actor
   }
   public void changeMana(int change)
   {
+    if(currentMana + change > maxMana) //uses same control loop system as in changeHealth
+      currentMana = maxMana;
+    else
     currentMana += change; //if magic is used, change is negative; if mana potion is used, change is positive
   }
   public void knockOut()
