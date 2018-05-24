b7791a3829bf63b1693617f48e989f3096abfe7d
uayevan01 <35662789+uayevan01@users.noreply.github.com>
2018-05-24 10:25:17 -0400

    stuff

--- Menu.java
+++ Menu.java
@@ -3,13 +3,17 @@ public class Menu
 {
   public static int attack(int strength, int defense, attack)
   {
-    return (int)((double)strength / defense * 800); //change the constant
+    return (int)((double(strength)/(100/(100 + defense)))); //change the constant
   }
   public static int magic(List<Ability> magicList)
   {
-    
+    DmgAbility.cast(magicList.get(
   }
   public static int item(List<Item> itemList)
   {
   
+  }
+  public static void getCommand()
+  {
+    
   }
