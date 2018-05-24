5d9b67f69ec203eeb297ffef6406b3c2d6bcf6dd
uayevan01 <35662789+uayevan01@users.noreply.github.com>
2018-05-24 10:02:56 -0400

    Menu java class, sitll need fixes

---
+++ Menu.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Menu
+{
+  public static int attack(int strength, int defense, attack)
+  {
+    return (int)((double)strength / defense * 800); //change the constant
+  }
+  public static int magic(List<Ability> magicList)
+  {
+    
+  }
+  public static int item(List<Item> itemList)
+  {
+  
+  }
