00c713f428ca06704b9834fb26cc9aab138e05d3
FinnFlam <39463740+FinnFlam@users.noreply.github.com>
2018-05-24 10:25:45 -0400

    Update Slime.java

--- Slime.java
+++ Slime.java
@@ -7,10 +7,19 @@ public class Slime extends Enemy
     int speed;
     public Slime(int level)
     {
-      maxHealth = 7 + (level*2) + (int)((level*Math.Random())+1);
+      maxHealth = 7 + (level*2) + (int)((level*Math.Random()));
       currentHealth = maxHealth;
       strength=5+level+((int)((level*Math.Random())+1));
       defense=2+level;
       speed=1+((int)((level*Math.Random())+1));
+    }
+    public void act()
+    {
+      
+      currentHealth+=(maxHealth*.2)
+    }
+    public int baseAttack(Actor recipient)
+    {
+        return (int) (((double) strength/recipient)*)
     }
  }
