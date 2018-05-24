cb7c3e4ecfdd7486310aae3a71a7e5b628b954fb
lburke01 <lburkerobotics@gmail.com>
2018-05-24 10:04:43 -0400

    Update Ability.java

--- Ability.java
+++ Ability.java
@@ -1,15 +1,19 @@
 public class Ability
 {
+  String name;
+  String desc;
   int rate;
   int manaCost;
   Actor caster;
   Actor recipient;
-  public Ability(double r, int mC, Actor a, Actor rC)
+  public Ability(double r, int mC, Actor a, Actor rC, String n, String d)
   {
     rate = r;
     manaCost = mC;
     caster = a;
     recipient = rC;
+    name = n;
+    desc = d;
   }
   public void cast()
   {
