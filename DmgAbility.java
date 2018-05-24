126780e872caef2320f27725194a0582bbd702d5
lburke01 <lburkerobotics@gmail.com>
2018-05-24 10:05:54 -0400

    Update DmgAbility.java

--- DmgAbility.java
+++ DmgAbility.java
@@ -1,1 +1,13 @@
-
+public class DmgAbility extends Ability
+{
+  public DmgAbility(double r, int mC, Actor a, Actor rC, String n, String d)
+  {
+    super(r, mC, a, rC, n, d);
+  } 
+  public void cast()
+  {
+    super();
+    int dmg = -1 * ((caster.magic / recipient.defense) * r);
+    recipient.changeHealth(dmg);
+  }
+}
