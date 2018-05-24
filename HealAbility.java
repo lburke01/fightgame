36e11484d99c5c84f0407e18ecf3bb2f26ecf218
lburke01 <lburkerobotics@gmail.com>
2018-05-24 10:14:46 -0400

    Update HealAbility.java

--- HealAbility.java
+++ HealAbility.java
@@ -1,1 +1,12 @@
-
+public class HealAbility extends Ability
+{
+  public HealAbility(double r, int mC, Actor a, Actor rC, String n, String d)
+  {
+    super(r, mC, a, rC, n, d);
+  }
+  public void cast()
+  {
+    super();
+    int heal = actor.magic * rate;
+    recipient.changeHealth(heal);
+  }
