e744bc866de69c7d63c2edfa352f347a96b0410c
FinnFlam <39463740+FinnFlam@users.noreply.github.com>
2018-06-05 10:22:50 -0400

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -1,18 +1,35 @@
-public class Enemy extends Actor
+public abstract class Enemy extends Actor
 {
-	  public void changeHealth(int change)
-	  {
-	    if(currentHealth + change > maxHealth) //Prevents over-healing by setting currentHealth to maxHealth if the sum is greater than maxHealth
-	      currentHealth = maxHealth;
-	    else
-	      currentHealth += change; //if change is damage, it is a negative value; if change is healing, it is a positive value
-	    
-	  }
-	  public void changeMana(int change)
-	  {
-	    if(currentMana + change > maxMana) //uses same control loop system as in changeHealth
-	      currentMana = maxMana;
-	    else
-	      currentMana += change; //if magic is used, change is negative; if mana potion is used, change is positive
-	  }
+	int target;
+//    public Enemy();
+
+	public Environment act(Environment e)
+	{
+		target=findTarget(e);
+		e.getPlayers().get(target).changeHealth(-1*baseAttack(baseAttack(e.get(target).getDefense())));
+		return e;
+	}
+	public int getMaxHealth()
+	{
+		return maxHealth;
+	}
+	public int baseAttack(int d)
+    {
+        return (int)(((double)strength)*(100.0/(100 + d)));
+    }
+    public int findTarget(Environment e)
+    {
+    	target=Math.random()*e.getPlayers().size();
+    	for(int i=0;i<e.getPlayers().size();i++)
+    	{
+    		if(e.getPlayers().get(i).getHealth()<((int)e.getPlayers().get(i).getMaxHealth()*.3))
+    		{
+    			if(e.getPlayers().get(i).getHealth()<e.getPlayers().get(target).getHealth())
+    			{
+    				target=i;
+    			}
+    		}
+    		
+    	}
+    }
 }
