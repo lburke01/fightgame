a4dbdba11b5d719accb605e488165f81de790ca7
uayevan01 <35662789+uayevan01@users.noreply.github.com>
2018-06-09 18:51:58 -0400

    Add files via upload

--- Command/Command.java
+++ Command/Command.java
@@ -1,1 +1,30 @@
 
+public abstract class Command 
+{
+	public final static int TARGET_ONE = 0;
+	public final static int TARGET_ALL = 1;
+	
+	protected String name;
+	protected Actor caster;
+	protected Actor[] targets;
+	protected Actor currentTarget;
+	protected int currentIndex;
+	protected int damage;
+	protected boolean groupTarget = true; //true = enemies, false = allies
+	public Command(Actor a, Actor[] ab)
+	{
+		caster = a;
+		targets = ab;
+		currentIndex = 0;
+		currentTarget = targets[currentIndex];
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getDamage()
+	{
+		return damage;
+	}
+	abstract public int calculateEffect();
+}
